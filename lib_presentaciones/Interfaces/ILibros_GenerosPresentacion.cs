b86f23028355f5af7c7b4bf5b853cddc15b14e05
Leidy <leidykavelez@gmail.com>
2025-05-10 18:06:50 -0500

    se agrega a libros_aut y libros_gen

---
+++ lib_presentaciones/Interfaces/ILibros_GenerosPresentacion.cs
@@ -0,0 +1,13 @@
+﻿using lib_dominio.Entidades;
+
+namespace lib_presentaciones.Interfaces
+{
+    public interface ILibros_GenerosPresentacion
+    {
+        Task<List<Libros_Generos>> Listar();
+        Task<List<Libros_Generos>> PorUsuario(Libros_Generos? entidad);
+        Task<Libros_Generos?> Guardar(Libros_Generos? entidad);
+        Task<Libros_Generos?> Modificar(Libros_Generos? entidad);
+        Task<Libros_Generos?> Borrar(Libros_Generos? entidad);
+    }
+}
